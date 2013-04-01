892a1b75c14f75db747669f0e0ad1d5ffc5c033c
Brylie Oxley <brylie@gnumedia.org>
2013-04-01 15:33:35 -0700

    First commit.

---
+++ public_html/js/formCalculator.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Form elements
+var ageGroupList = "custom_list-5153b344f2fa7";
+var attendanceRadio = "input:radio[name=custom_list-51512c144f9fb]:checked";
