f61b1f3c91c55c1f14abc7448b9cf0a92f042969
Brylie Oxley <brylie@gnumedia.org>
2013-04-01 20:58:44 -0700

    added variables for commuter and payment form elements

--- public_html/js/formCalculator.js
+++ public_html/js/formCalculator.js
@@ -1,22 +1,35 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// Static variables
+date = new Date(); // Date used for discount calculation
 
 // Form elements
-var ageGroupList = "custom_list-5153b344f2fa7";
+var ageGroupList = "#custom_list-5153b344f2fa7";
 var attendanceRadio = "input:radio[name=custom_list-51512c144f9fb]:checked";
 
 //Commuter form elements
-var monCheckbox = "custom_list-51512492726cc-0";
-var tueCheckbox = "custom_list-51512492726cc-1";
-var wedCheckbox = "custom_list-51512492726cc-2";
-var thuCheckbox = "custom_list-51512492726cc-3";
-var friCheckbox = "custom_list-51512492726cc-4";
-var satCheckbox = "custom_list-51512492726cc-5";
+var monCheckbox = "#custom_list-51512492726cc-0";
+var tueCheckbox = "#custom_list-51512492726cc-1";
+var wedCheckbox = "#custom_list-51512492726cc-2";
+var thuCheckbox = "#custom_list-51512492726cc-3";
+var friCheckbox = "#custom_list-51512492726cc-4";
+var satCheckbox = "#custom_list-51512492726cc-5";
 
+var commuterAmountOfDays = "#text-515525726e966";
+var commuterFeesSubtotal = "#text-5155132f62cc6";
 // Overnight form elements
 
+
+// Final fees and payment elements
+var firstChoiceSubTotal = "#text-5158cf384d951";
+var secondChoiceSubTotal = "#text-5158cf3c9326f";
+var totalFeesFromAbove = "#text-515621f626b6e";
+var earlyDiscountRadio = "input:radio[name=custom_list-5156340d5ee07]:checked";
+// TODO: set the earlyDiscountRadio based on Date()
+var subTotalFees = "#text-515621f92963c";
+var totalFeesDue = "#text-5156220324787";
+
+//Payment section
+amountEnclosed = "#text-515622061f15c";
+balanceDueOnArrival = "#text-51563412d9e9e"
 
 /* Check to see if a checkbox is checked
  * jQuery("#checkbox-id").is(":checked")
