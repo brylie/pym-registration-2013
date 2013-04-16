3badfbbe65305245f25cc029087be52d64773d76
Brylie Oxley <brylie@gnumedia.org>
2013-04-16 14:49:27 -0700

    added function to calculate commuter subtotal (Fixes #4). Adjusted commuter fees (Fixes #16). Clean-up and comments.

--- js/formCalculator.js
+++ js/formCalculator.js
@@ -1,14 +1,14 @@
 /* jshint laxcomma: true */
     // Static variables
     var date = new Date(); // Date used for discount calculation
-
-    var commuterPrices = [
-        0 // null
-        ,0 // null
-        ,0 // 0 - 5
-        ,30 // 6 - 12
-        ,45 // 13 - 25
-        ,75 // 26 +
+    // Price sets
+    var commuterPrices = [ // [daily, discount, weekly]
+        0 // null (placeholder selection)
+        ,0 // null (placeholder selection)
+        ,[0, 0, 0] // >= 0 && <= 5
+        ,[30, 15, 125] // >= 6 && <= 12 
+        ,[45, 22.5, 200] // >= 13 && <= 25
+        ,[75, 37.5, 350] // >= 26
     ];
     var overnightAccommodationsPrices = [
         // Organized by type of accommodation
@@ -62,11 +62,9 @@
             , [750,null] // >= 26
         ]
     ];
-
     // Form elements
     var ageGroupList = "#custom_list-5153b344f2fa7";
     var attendanceRadio = "input:radio[name=custom_list-51512c144f9fb]:checked";
-
     //Commuter form elements
     var monCheckbox = "#custom_list-51512492726cc-0";
     var tueCheckbox = "#custom_list-51512492726cc-1";
@@ -88,78 +86,111 @@
     var overnightAttending = "input:radio[name=custom_list-51552d7af14b2]:checked";
     var overnightAttendingFull = "#custom_list-51552d7af14b2-0";
     var overnightAttendingPartial = "#custom_list-51552d7af14b2-1";
-
     // Partial fields
     var overnightPartialSessionArrival = "#custom_list-51512d65f3ccd";
     var overnightPartialSessionDeparture = "#custom_list-51512e25e75c3";
     var overnightPartialSessionAmountOfDays = "#text-5155020930b2e";
-
-
     // Overnight choices
     var overnightFirstChoiceSelect = "#custom_list-5151316b82122";
     var overnightFirstChoiceFeesSubtotal = "#text-5155c5a3c3e6f";
     var overnightSecondChoiceSelect = "#custom_list-5155391ed978e";
     var overnightSecondChoiceFeesSubtotal = "#text-5155c5a7b1cd9";
-
     // Roomate preferences
     var overnightRoommatePreferences = "#custom_list-515137f3e9e63";
-
     // Final fees and payment elements
     var firstChoiceSubTotal = "#text-5158cf384d951";
     var secondChoiceSubTotal = "#text-5158cf3c9326f";
     var totalFeesFromAbove = "#text-515621f626b6e";
     var earlyDiscountRadio = "input:radio[name=custom_list-5156340d5ee07]:checked";
     // TODO: set the earlyDiscountRadio based on Date() ?
-
     // Donation and discount
     var optionalDonation = "#text-515621fd28e1c";
     var financialAid = "#text-5156220059f2d";
-
     // Total and subtotal
     var subTotalFees = "#text-515621f92963c";
     var totalFeesDue = "#text-5156220324787";
-
     //Payment section
     var amountEnclosed = "#text-515622061f15c";
     var balanceDueOnArrival = "#text-51563412d9e9e";
-
+    // End of initial variables
+    // Begin form functions
     var countCommuterDays = function() {
-        var daysChecked = 0;
-
+      /* 
+       * count the number of commuter days checked
+       * return an integer
+       */
+        var numberOfDaysChecked = 0;
         for (var i=0;i < days.length;i++) {
           if (jQuery(days[i]).is(":checked")) {
-            daysChecked += 1;
-          }
-        }
-
-        return daysChecked;
+            numberOfDaysChecked += 1;
+          };
+        };
+        return numberOfDaysChecked;
     };
-
+    var determineCommuterDaysChecked = function() {
+      /* 
+       * return an array of day values
+       * based on commuter day checkboxes
+       * return array of numeric day values
+       */
+      var commuterDaysCheckedList = [];
+      for (var i=0;i < days.length;i++) {
+	if (jQuery(days[i]).is(":checked")) {
+	  commuterDaysCheckedList.push(parseInt(i));
+        };
+      };
+      return commuterDaysCheckedList;
+    }
     var updateCommuterAmountOfDaysField = function() {
+      /*
+       * Update the 'commuter amount of days' field
+       * based on the count of commuter days checked
+       * return null ?
+       */
         var numberOfDays = countCommuterDays();
         // update the form element
         jQuery(commuterAmountOfDays).val(numberOfDays);
         jQuery(commuterAmountOfDays).change();
     };
-
     var calculateCommuterFeesSubTotal = function() {
+      /*
+       * calculate the commuter fees subtotal
+       * based on the age group and number of days checked
+       * if five or more days are checked, default to weekly rate
+       * Saturday and Monday fees are based on the discount rate
+       * return float ?
+       */
         var ageGroup = parseInt(jQuery(ageGroupList).val(), 10);
         var numberOfDays = parseInt(jQuery(commuterAmountOfDays).val(), 10);
-        var subTotal = commuterPrices[ageGroup] * numberOfDays;
-        if (subTotal > 0) {
-            return subTotal;
-        } else if (ageGroup <= 2) { // children 0 - 5 or no selection
-            return 0;
+	var specificDays = determineCommuterDaysChecked();
+	var subTotal = 0; // integer
+	
+        if (numberOfDays < 5) { // sum daily prices
+          for (i=0;i<days.length;i++) {
+	    if (specificDays[i] > 0 && specificDays[i] < 5) { // not Monday or Saturday
+	      subTotal += commuterPrices[ageGroup][0]; // add full day rate to subtotal
+	    } else if (specificDays[i] === 0 || specificDays[i] === 5) { // is Monday or Saturday
+	      subTotal += commuterPrices[ageGroup][1]; // add discount price to subtotal
+	    } else { // otherwise add zero (catch all)
+	      subTotal += 0;
+	    }
+	  };
+	} else if (numberOfDays >= 5 && numberOfDays <= 6) { // set to weekly rate
+	  subTotal = commuterPrices[ageGroup][2]; 
+	} else { // Catch all
+            subTotal = 0;
         }
-        else {
-            return null;
-        }
+        return subTotal;
     };
     var updateCommuterFeesSubTotalField = function() {
+      /*
+       * Update the 'commuter sub total' field
+       * based on the commuter fees subtotal calculation
+       * return null ?
+       */
         var subTotal = calculateCommuterFeesSubTotal();
         jQuery(commuterFeesSubTotal).val(subTotal);
     };
-
     var calculatePartialSessionLength = function() {
         var lengthOfStay;
         // type cast both values with parseInt() to perform subtraction later
@@ -179,12 +210,10 @@
             return null;
         }
     };
-
     var updateAmountOfDaysField = function() {
         var lengthOfStay = calculatePartialSessionLength();
         jQuery(overnightPartialSessionAmountOfDays).val(lengthOfStay);
     };
-
     var modifyAccommodationsChoices = function() {
         // check whether overnight attending full or partial is selected
         // Set the select values to zero (no choice)
@@ -204,8 +233,7 @@
             jQuery(overnightSecondChoiceSelect + " option[value=8]").hide();
         }
         return null;
-    };
-    
+    };   
     var updateRoommatePreferencesField = function() {
         var firstChoiceAccommodations = parseInt(jQuery(overnightFirstChoiceSelect).val(), 10);
         var overnightRoommatePreferencesField = jQuery(overnightRoommatePreferences);
@@ -245,5 +273,4 @@
          jQuery(overnightAttendingFull).click(updateRoommatePreferencesField);
          jQuery(overnightAttendingPartial).click(updateRoommatePreferencesField);
     };
-
     jQuery(document).load(attachEvents());
